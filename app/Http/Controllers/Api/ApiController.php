6d966bc8076a7aaff99afe0e83c47aa7a597a5cd
Luca Cassano <lucacassano96@gmail.com>
2023-08-21 17:41:37 +0200

    Api controller

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ApiController extends Controller
+{
+    public function testApi(){
+        return response() -> json([
+            "message" => "Hello world"
+        ]);
+    }
+
+
+    public function projectsIndex(){
+
+        $projects = Project :: with("type")-> paginate(4);
+        return response() -> json([
+            "projects" => $projects
+        ]);
+    }
+}
